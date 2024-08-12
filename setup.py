c151f4909ffb61e29bb58ee748852550616e6c4f
“NagarjunaReddy2” <sunkireddynagarjuna222@gmail.com>
2024-08-12 20:33:17 +0530

    Update setup.py with new code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+
+    name = 'SalesStorePrediction',
+    version = '0.0.1',
+    author = 'Nagarjuna Sunkireddy',
+    author_email = 'sunkireddynagarjuna@222gmail.com',
+    packages = find_packages()
+
+)
